5a74b734be164a10f7cbf69df9bf8f1cf6b363a4
Felipe Rostirolla <felipe.rost.f@gmail.com>
2024-08-16 16:09:30 -0300

    working on the forum api

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -2,8 +2,19 @@ import Sidebar from "../components/sidebar"
 import RightSidebar from "../components/rightSidebar"
 import Link from 'next/link';
 import { Button } from "@/components/ui/button";
+import forum_posts from '../data/communityForum.json'
+
 
 export default function Community() {
+  // const forum_posts = fetch('../data/communityForum.json');
+  interface Post { 
+    post_id: number; 
+    post_title: string;
+    post_body: string;
+    post_creator: string;
+    post_date: string;
+    post_comments: any;
+  }
   return (
     <>
       <div className="@apply flex w-[1024px] h-[768px] bg-[#2a2a2a] border-2 border-solid border-[#444]">
@@ -11,12 +22,28 @@ export default function Community() {
           <div className="@apply flex-1 overflow-y-auto p-5">
               <div className="@apply text-center mb-5">
                   <h1>Deadwell Community Forum</h1>
-                  {/* <div id="timer">Next effect in: 120s</div> */}
               </div>
+
+              {/* <div>
+                {Object.keys(forum_posts).map((key, i) => (
+                  <p key={i}>
+                    <span>Key Name: {key}</span>
+                    <span>Value: {forum_posts[key]}</span>
+                  </p>
+                ))}
+              </div> */}
+              {/* <ul className='@apply list-none w-full p-0'>
+                {forum_posts.map(()=>(
+                    <li key={index}>
+                        <p>{item.title}</p>
+                    </li>
+                    ))}
+              </ul> */}
+                
 
 {/* later, here we will apply a logic to pull posts from our simulated api, communityForum.json*/}
 {/* read this to see if we can make this way: https://dev.to/vcpablo/js-mocking-a-rest-api-with-json-server-368 */}
-              <div className="@apply mb-5">
+              {/* <div className="@apply mb-5">
                   <h2>Highlighted Community Post</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, metus nec tincidunt placerat, risus arcu sollicitudin urna, nec pretium nisl nulla ac erat.</p>
                   <p>format around 5~6 posts orkut/reddit-style</p>
@@ -40,7 +67,7 @@ export default function Community() {
               <div className="@apply mb-5">
                   <h2>Highlighted Community Post 6</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, metus nec tincidunt placerat, risus arcu sollicitudin urna, nec pretium nisl nulla ac erat. Nulla condimentum, metus nec tincidunt placerat, risus arcu sollicitudin urna, nec pretium nisl nulla ac erat.</p>
-              </div>
+              </div> */}
 
               <p>And, at the end:</p>
               <Button>
